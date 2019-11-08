435e32bc707ed3b6ce6f0e90882bbc9252f1813c
Sterling Watts <SterlingDWatts@gmail.com>
2019-11-07 17:22:48 -0800

    Added third test and saved snapshot

--- src/Messages.test.js
+++ src/Messages.test.js
@@ -4,15 +4,22 @@ import Messages from './Messages';
 import renderer from 'react-test-renderer';
 import { exportAllDeclaration } from '@babel/types';
 
-it('renders without creaching', () => {
+it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Messages />, div);
     ReactDOM.unmountComponentAtNode(div);
 });
 
 it('renders the UI as expected', () => {
-const tree = renderer
-    .create(<Messages name="Messages" unread={4}/>)
-    .toJSON();
-expect(tree).toMatchSnapshot();
+    const tree = renderer
+        .create(<Messages name="Messages" unread={4}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();  
+});
+  
+it('renders the UI as expected with no unreads', () => {
+    const tree = renderer
+        .create(<Messages name="Messages" unread={0}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();  
 });
