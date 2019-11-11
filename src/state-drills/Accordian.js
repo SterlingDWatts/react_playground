0c4887b9cf3f497fc67b15bd74cd8fc09f1d6f3f
Sterling Watts <SterlingDWatts@gmail.com>
2019-11-10 18:05:41 -0800

    Finished basic accordian. Made test suite

--- src/state-drills/Accordian.js
+++ src/state-drills/Accordian.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Accordian extends Component {
+    static defaultProps = {
+        section: []
+    }
+    state = {
+        currentButton: null,
+    }
+    handleButtonClick(index) {
+        this.setState({ currentButton: index })
+    }
+    renderContent(index) {
+        if (index === this.state.currentButton) {
+            return (
+                <p>{this.props.section[index].content}</p>
+            )
+        } else {
+            return <div></div>
+        }
+    }
+    renderListItems() {
+        return this.props.section.map((section, index) => (
+            <li key={index}>
+                <button key={index} onClick={() => this.handleButtonClick(index)}>{section.title}</button>
+                {this.renderContent(index)}
+            </li>
+        ))
+    }
     render() {
         return (
-            <div></div>
+            <ul>
+                {this.renderListItems()}
+            </ul>
         )
     }
 }
--- src/state-drills/Accordian.test.js
+++ src/state-drills/Accordian.test.js
@@ -5,9 +5,42 @@ import toJson from 'enzyme-to-json';
 import Accordian from './Accordian';
 
 describe('Accordian Component', () => {
+    const sections = [
+        {
+            title: 'Section 1',
+            content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        },
+        {
+            title: 'Section 2',
+            content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+        },
+        {
+            title: 'Section 3',
+            content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+        }
+    ]
     it('renders without errors', () => {
         const div = document.createElement('div');
         ReactDOM.render(<Accordian />, div);
         ReactDOM.unmountComponentAtNode(div);
+    })
+    it('renders an empty ul given no sections', () => {
+        const wrapper = shallow(<Accordian />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
+    it('renders no section as active by default', () => {
+        const wrapper = shallow(<Accordian section={sections}/>)
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
+    it('opens a section when the button is clicked', () => {
+        const wrapper = shallow(<Accordian section={sections}/>)
+        wrapper.find('button').at(0).simulate('click')
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
+    it('only has the last the section open from the last button clicked', () => {
+        const wrapper = shallow(<Accordian section={sections}/>)
+        wrapper.find('button').at(0).simulate('click')
+        wrapper.find('button').at(1).simulate('click')
+        expect(toJson(wrapper)).toMatchSnapshot()
     })
 })
